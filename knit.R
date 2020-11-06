cad2f218d255b754f531fc4438e9f239a71e9b5b
mca91 <martin.emwikts@googlemail.com>
2020-11-06 16:23:05 +0100

    adjusted styles in DT

--- knit.R
+++ knit.R
@@ -1,2 +1,2 @@
-output_dir <- "../output"
-rmarkdown::render("index.Rmd", output_dir = "docs")
+output_dir <- "~/Git projects/ECMTX_course_overview/output"
+rmarkdown::render("~/Git projects/ECMTX_course_overview/index.Rmd", output_dir = "docs")
