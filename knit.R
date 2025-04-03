9de420982a003aaba7e93403f63ccf64e2880b28
Klenke <57100601+jens-klenke@users.noreply.github.com>
2025-04-03 11:56:46 +0200

    knit rendering to docs

--- knit.R
+++ knit.R
@@ -1,2 +1,5 @@
 output_dir <- "~/git_projects/CourseOverview/docs"
 rmarkdown::render("~/git_projects/CourseOverview/index.Rmd", output_dir = output_dir)
+
+# Jens Windows 
+rmarkdown::render("index.Rmd", output_dir = here::here('docs'))
